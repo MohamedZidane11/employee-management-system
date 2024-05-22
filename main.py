7fafd33ce03dccbd5265a0300b33086c243a9d50
Mohamed Zidane <mohamedzidane11@gmail.com>
2024-05-22 08:08:51 +0300

    adding and customizing action buttons

--- main.py
+++ main.py
@@ -4,20 +4,23 @@ from tkinter import ttk
 import tkinter as tk
 from tkinter import messagebox
 
-# ================= creating main window =================
+# ================= main window settings =================
 app = customtkinter.CTk()
 app.title("Employees Management System")
 app.after(201, lambda: app.iconbitmap('J:\Python Projects\employee management system tkinter\manage-employee.ico'))
-app.geometry('800x420')
+app.geometry('860x420')
 app.config(bg='#161c25')
 app.resizable(False, False)
 
 font1 = ('Arial', 20, 'bold')
 font2 = ('Arial', 12, 'bold')
 
-# ================= placing titles and entry =================
+bg_color1 = '#161c25'
+fg_color1 = '#fff'
+hover_color1 = '#00850b'
 
-id_label = customtkinter.CTkLabel(app, font=font1, text='ID', text_color='#fff', bg_color='#161c25')
+# ================= labels and entry fields =================
+id_label = customtkinter.CTkLabel(app, font=font1, text='ID', text_color='#fff', bg_color=bg_color1)
 id_label.place(x=20, y=20)
 id_entry = customtkinter.CTkEntry(app, font=font1, text_color='#000', fg_color='#fff', border_color='#0c9295', border_width=2, width=180)
 id_entry.place(x=100, y=20)
@@ -46,6 +49,17 @@ status_label = customtkinter.CTkLabel(app, font=font1, text='Status', text_color
 status_label.place(x=20, y=260)
 status_entry = customtkinter.CTkEntry(app, font=font1, text_color='#000', fg_color='#fff', border_color='#0c9295', border_width=2, width=180)
 status_entry.place(x=100, y=260)
+
+
+# ================= labels and entry fields =================
+add_button = customtkinter.CTkButton(app, font=font1, text='Add Employee', text_color='#fff', fg_color='#05a312', hover_color=hover_color1, bg_color=bg_color1, cursor='hand2', corner_radius=15, width=260)
+add_button.place(x=20, y=310)
+clear_button = customtkinter.CTkButton(app, font=font1, text='New Employee', text_color='#fff', fg_color='#161c25', hover_color='#ff5002', bg_color=bg_color1, border_width=2, border_color='#f15704', cursor='hand2', corner_radius=15, width=260)
+clear_button.place(x=20, y=360)
+update_button = customtkinter.CTkButton(app, font=font1, text='Update Employee', text_color='#fff', fg_color='#161c25', hover_color='#ff5002', bg_color=bg_color1, border_width=2, border_color='#f15704', cursor='hand2', corner_radius=15, width=260)
+update_button.place(x=300, y=360)
+delete_button = customtkinter.CTkButton(app, font=font1, text='Delete Employee', text_color='#fff', fg_color='#e40404', hover_color='#ae0000', bg_color=bg_color1, border_color='#e40404', cursor='hand2', corner_radius=15, width=260)
+delete_button.place(x=580, y=360)
 
 app.mainloop()
 
